903893c711a9c885dc6c0ce7ea170ee3393f5bfe
Cris <12250662+CristianCrane@users.noreply.github.com>
2022-12-16 22:18:57 -0500

    add springboot starter

---
+++ src/main/java/com/marvelousmacros/app/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.marvelousmacros.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloWorldController {
+
+    @GetMapping
+    public String greet() {
+        return "hi :P";
+    }
+}
